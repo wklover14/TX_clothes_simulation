1bebedeee4328f1e19ef697c9a10ad3112e094d1
gwatchok <gwatchok@etu.utc.fr>
2024-07-30 22:37:14 +0000

    Physical balance gone for the flag Need some check with the Table

--- include/const.h
+++ include/const.h
@@ -15,20 +15,20 @@
  ************************************/
 
 // SPACE CONST
-#define     SPACING                     0.01f                           //   standard spacing between two points on the same axis
+#define     SPACING                     1.0f                           //   standard spacing between two points on the same axis
 
 // MESH CONST
 #define     Mu                          1.00f                           // Mass of a point
-#define     C_DIS                       0.0f                            // Damping coefficient
+#define     C_DIS                       0.90f                            // Damping coefficient
 #define     C_VI                        0.0f                             // Viscous coefficient
 
 // SPRINGS CONST
 #define     MAX_SPRINGS_PER_POINT       12
-#define     STIFFNESS_H                 100.01f                          // stiffess of a horizontal spring
-#define     STIFFNESS_V                 100.01f                          // stiffess of a vertical spring 
-#define     STIFFNESS_D                 100.01f                          // stiffess of a diagonal spring
+#define     STIFFNESS_H                 10.0f                          // stiffess of a horizontal spring
+#define     STIFFNESS_V                 10.0f                          // stiffess of a vertical spring 
+#define     STIFFNESS_D                 10.0f                          // stiffess of a diagonal spring
 
 // FIXED POINT
-#define     RADIUS                      0.025f                            // For the table, the radius of the circle containing fixed points
+#define     RADIUS                      0.25f                            // For the table, the radius of the circle containing fixed points
 
 #endif // !CONST_H
--- src/main.c
+++ src/main.c
@@ -7,26 +7,28 @@
 int main(int argc, char** argv)
 {
     Mesh* m = (Mesh*) malloc(sizeof(Mesh));
-        meshType type = TABLE;
-        initMesh(m, 50, 50, type);
+    meshType type = FLAG;
+    initMesh(m, 50, 50, type);
     log_info("The number of springs in this network is %d", numberOfSprings(m->n, m->m));
 
-    float delta_t = 0.02f;
-    unsigned int count = 400;
+    float delta_t = 0.1f;
+    unsigned int count = 7000;
     char poly_file_name[256];
     char grid_file_name[256];
 
-    log_info("Starting file generation: delta_time=%.3f number_of_file=%d", delta_t, count);
+    int step = 15;
+    log_info("Starting file generation: delta_time=%.3f number_of_file=%d", delta_t, count/step);
     clock_t start_time = clock();  // Start the timer
     for(unsigned int i=0; i< count; i++)
     {
+        if( i%step == 0){
+            snprintf(poly_file_name, sizeof(poly_file_name), "vtk_poly/mesh_poly_%03u.vtk", i);
+            snprintf(grid_file_name, sizeof(grid_file_name), "vtk_grid/mesh_grid_%03u.vtk", i);
+            convertMeshToPolyVTK(m, poly_file_name);
+            convertMeshToGridVTK(m, grid_file_name);
+        }
+        
         updatePosition(m, delta_t, type);
-        snprintf(poly_file_name, sizeof(poly_file_name), "vtk_poly/mesh_poly_%03u.vtk", i);
-        snprintf(grid_file_name, sizeof(grid_file_name), "vtk_grid/mesh_grid_%03u.vtk", i);
-
-        convertMeshToPolyVTK(m, poly_file_name);
-        convertMeshToGridVTK(m, grid_file_name);
-    
     }
     clock_t end_time = clock();  // End the timer
     double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;  // Calculate elapsed time in seconds
